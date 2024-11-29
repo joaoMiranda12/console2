5c1631bb95b2519e17eb87d41a0e73cb0a0ee422
Joao miranda <joaomiranda.201932@epff-intep.pt>
2024-11-29 13:05:38 +0000

    Add files via upload

---
+++ switch/ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class console2
+{
+    public static void Main(string[] args)
+    {
+        int grade = 15;
+        switch (grade)
+        {
+
+            case >= 18 and 20:
+                Console.WriteLine("Excelente");
+                break;
+
+            case >= 14 and 17:
+                Console.WriteLine("bom");
+                break;
+
+            case >= 10 and 13:
+                Console.WriteLine("suficiente");
+                break;
+
+            case >= 0 and 9:
+                Console.WriteLine("infuciente");
+                break;
+            default:
+                Console.WriteLine("Nota invalida");
+                break;
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
